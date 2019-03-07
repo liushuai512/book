97492ada3954e5b825a4388bf711a51dd7db34d8
liushuai512 <1570448684@qq.com>
2019-03-07 14:47:58 +0800

    调试

--- vue-imooc-ebook/src/components/ebook/EbookReader.vue
+++ vue-imooc-ebook/src/components/ebook/EbookReader.vue
@@ -1,38 +1,40 @@
 <template>
   <div class="ebook-reader">
-    <div id="read"></div> 
+    <div id="read"></div>
   </div>
 </template>
 
 
 <script>
-import { mapGetters } from 'vuex'
-import Epub from 'epubjs'
-global.ePub = Epub  
+import { mapGetters } from "vuex";
+import Epub from "epubjs";
+global.ePub = Epub;
 export default {
   computed: {
-    ...mapGetters(['fileName'])
+    ...mapGetters(["fileName"])
   },
   methods: {
-    initEpub(){
-      const url = 'http://localhost:8081/epub/' + this.fileName + '.epub'
+    initEpub() {
+      const url = "http://localhost:8081/epub/" + this.fileName + ".epub";
       this.book = new Epub(url);
-      this.rendition = this.book.renderTo('read',{
-        width:innerWidth,
-        height:innerHeight,
-        method:'default'
-      })
-      this.rendition.display()
+      this.rendition = this.book.renderTo("read", {
+        width: innerWidth,
+  height: innerHeight,
+        method: "default"
+      });
+      this.rendition.display();
     }
   },
-  mounted () {
-    const fileNameAA = this.$route.params.fileName
-    const fileName = this.$route.params.fileName.split('|').join('/')
-    this.$store.dispatch('setFileName', fileName).then(()=>{this.initEpub()})
-  }   
-}
+  mounted() {
+    const fileNameAA = this.$route.params.fileName;
+    const fileName = this.$route.params.fileName.split("|").join("/");
+    this.$store.dispatch("setFileName", fileName).then(() => {
+      this.initEpub();
+    });
+  }
+};
 </script>
 
 <style lang="scss" rel="stylesheet/scss" scope>
-  @import "../../assets/styles/global.scss"
+@import "../../assets/styles/global.scss";
 </style>
